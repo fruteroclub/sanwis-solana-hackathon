6d4b7695d33f22c08ebe294e7fed0ef552f13f84
GerGodfrey <luis-godfrey@hotmail.com>
2023-02-23 21:53:16 -0600

    cambios en assets

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,11 @@
 import Products from '../components/Products'
 import SiteHeading from '../components/SiteHeading'
-import { logo_2 } from './assets'
 
 export default function HomePage() {
   return (
     <div className="flex flex-col gap-8 max-w-4xl items-stretch m-auto pt-24">
       <SiteHeading>Sanwis</SiteHeading>
-      {/* <img src={logo_2} alt="logo" className='w-[261px] h-[62px]' /> */}
+      
 
       <Products submitTarget='/checkout' enabled={true} />
     </div>
