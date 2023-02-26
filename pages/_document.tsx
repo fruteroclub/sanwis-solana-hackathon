9aeef86e8661f32eb617a6b8f8e2ebe5467c217f
haycarlitos <carlos.castilloperera@gmail.com>
2023-02-26 13:43:33 -0800

    styles: add google fonts

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html>
-      <Head />
+      <Head>
+      <link
+        href="https://fonts.googleapis.com/css2?family=League+Spartan"
+        rel="stylesheet"
+      />
+      </Head>
       <body className='bg-blue-50'>
         <Main />
         <NextScript />
