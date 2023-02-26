a1f969982591d2ca6d980fbe8d4ff9032b4446e3
GerGodfrey <luis-godfrey@hotmail.com>
2023-02-26 12:00:48 -0600

    webpack for sheets

--- next.config.js
+++ next.config.js
@@ -4,4 +4,14 @@ module.exports = {
   images: {
     domains: ['images.unsplash.com'],
   },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback.fs = false;
+      config.resolve.fallback.tls = false;
+      config.resolve.fallback.net = false;
+      config.resolve.fallback.child_process = false;
+    }
+
+    return config;
+  },
 }
