7cbd2ff0a95bc1e3305871bab7246c1f0319a75c
GerGodfrey <luis-godfrey@hotmail.com>
2023-02-25 18:26:40 -0600

    url en json

--- lib/products.ts
+++ lib/products.ts
@@ -6,6 +6,7 @@ export const products = [
     unitName: 'Sanwis', // shows after the price, eg. 0.05 SOL/box
     priceSol: 0.05,
     priceUsd: 2,
+    url : "https://images.unsplash.com/photo-1481070414801-51fd732d7184?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=724&q=80"
   },
   {
     id: 's-gigante',
@@ -13,7 +14,9 @@ export const products = [
     description: 'Sanwis, con cuatro panes, jamón de pavo en tres secciones, lechuga, y aderezo estilo ranch.',
     unitName: 'Sanwis',
     priceSol: 0.1,
-    priceUsd: 4.2,
+    priceUsd: 4.2, 
+    url : "https://images.unsplash.com/photo-1639667852145-466e29aa49fd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80", 
+
   },
   {
     id: 's-para-dos',
@@ -22,6 +25,7 @@ export const products = [
     unitName: 'Sanwis',
     priceSol: 0.1,
     priceUsd: 4,
+    url : "https://images.unsplash.com/photo-1639667911189-700bd2029f5b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
   },
   {
     id: 's-gourmet',
@@ -30,5 +34,6 @@ export const products = [
     unitName: 'Sanwis',
     priceSol: 0.1,
     priceUsd: 3.5,
+    url : "https://images.unsplash.com/photo-1628191010210-a59de33e5941?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
   }
 ]
